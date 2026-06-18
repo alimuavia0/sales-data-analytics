5c54b498e62a5dfe1e98823e66114b94f84baeb2
Abdul Raheem <alimuaviashah100010@gmail.com>
2026-06-18 11:07:02 +0500

    Add files via upload

---
+++ notebooks/p.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+a = [1,2,3,4,5]
+b = [2,8,90,4,56]
+d = pd.Series(a+b)
+print(a)
+# s = sns.barplot(a,b)
